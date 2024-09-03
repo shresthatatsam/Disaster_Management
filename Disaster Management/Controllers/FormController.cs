a98913e63c545bcf15f97bcf846d9881524418fc
Tatsam <tatsamshrestha93@gmail.com>
2024-09-03 08:17:45 -0700

    added new forms with htmlpartial

---
+++ Disaster Management/Controllers/FormController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Disaster_Management.Controllers
+{
+    public class FormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
